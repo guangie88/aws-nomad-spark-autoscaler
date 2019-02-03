d2259053c2281311d13e916e7426d9924febb81e
Chen Weiguang <chen.weiguang@gmail.com>
2019-02-03 10:22:38 +0800

    Add parsing steps and log

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,29 @@ struct Config {
     spark_master_url: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct MasterInfo {
+    completedapps: Vec<CompletedAppsInfo>,
+    workers: Vec<WorkerInfo>,
+    activedrivers: Vec<String>,
+    completeddrivers: Vec<String>,
+    status: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct CompletedAppsInfo {
+    id: String,
+    starttime: u64,
+    name: String,
+    cores: u32,
+    user: String,
+    memoryperslave: u64,
+    submitdate: String,
+    state: String,
+    duration: u64,
+}
+
+#[derive(Deserialize, Debug)]
 struct WorkerInfo {
     id: String,
     host: String,
@@ -25,7 +48,14 @@ struct WorkerInfo {
 
 fn main() -> Result<(), Box<Error>> {
     let config = envy::from_env::<Config>()?;
-    println!("{}", reqwest::get(&config.spark_master_url)?.text()?);
+    let spark_master_json_raw = reqwest::get(&config.spark_master_url)?.text()?;
+
+    // println!("{}", spark_master_json_raw);
+
+    let spark_master_info: MasterInfo = serde_json::from_str(&spark_master_json_raw)?;
+    let worker_infos = &spark_master_info.workers;
+
+    println!("{:#?}", worker_infos);
 
     Ok(())
 }
