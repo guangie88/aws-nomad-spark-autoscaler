180ae49612e09531682cc55b22a4d66633fe0eb8
Chen Weiguang <chen.weiguang@gmail.com>
2019-02-14 17:28:42 +0800

    Try to do json_query to change values

--- aws-nomad/src/config.rs
+++ aws-nomad/src/config.rs
@@ -1,4 +1,16 @@
 #[derive(Deserialize, Debug)]
 pub struct AwsNomadBackend {
-    nomad_job_per_aws_ec2: u32,
+    pub nomad_url: String,
+    pub nomad_job_name: String,
+    pub nomad_job_per_aws_ec2: u32,
+    pub scaling_mode: ScalingMode,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type", content = "value")]
+pub enum ScalingMode {
+    #[serde(rename = "doubling")]
+    Doubling,
+    #[serde(rename = "absolute")]
+    Absolute(u32),
 }
--- aws-nomad/src/lib.rs
+++ aws-nomad/src/lib.rs
@@ -2,11 +2,28 @@
 extern crate serde_derive;
 
 use failure::Error;
+use reqwest;
 
 pub mod config;
 pub use config::AwsNomadBackend;
 
-pub fn scale_up(_config: &AwsNomadBackend) -> Result<(), Error> {
+pub fn scale_up(config: &AwsNomadBackend) -> Result<(), Error> {
+    let nomad_url = reqwest::Url::parse(&config.nomad_url)?;
+    let spark_worker_url =
+        nomad_url.join(&format!("/v1/job/{}", &config.nomad_job_name))?;
+
+    let mut spark_worker_response = reqwest::get(spark_worker_url)?;
+    let spark_worker_response_text = spark_worker_response.text()?;
+
+    let spark_worker_info_json =
+        serde_json::from_str(&spark_worker_response_text)?;
+
+    // NEW_JOB=$(curl -s https://nomad.locus.rocks/v1/job/spark-worker | jq ".TaskGroups[].Count *= 2" | jq ".VaultToken=\"`cat ~/.vault-token`\"")
+    let _str_res = json_query::run(
+        r#".TaskGroups[].Count *= 2 | .VaultToken="`cat ~/.vault-token`""#,
+        spark_worker_info_json,
+    );
+
     Ok(())
 }
 
